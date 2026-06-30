b991b2b7303eb2c4d756911f128bb0289b53ed4c
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2026-06-29 20:50:04 -0400

    Adiciona codigoPais para Curacao

--- scripts/corrigir-bandeiras.sql
+++ scripts/corrigir-bandeiras.sql
@@ -1,2 +1,3 @@
 UPDATE selecoes SET "codigoPais" = 'kr' WHERE nome = 'Coreia do Sul';
 UPDATE selecoes SET "codigoPais" = 'cz' WHERE nome = 'República Tcheca';
+UPDATE selecoes SET "codigoPais" = 'cw' WHERE nome = 'Curaçao';
