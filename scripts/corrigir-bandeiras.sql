4cdad37a56d0fe98acf4b445ad9ea19de8c2e15a
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2026-06-30 07:24:25 -0400

    Adiciona pagina de detalhes dos estadios com rota por slug; descricao, historia no DB; cards linkaveis na lista

--- scripts/corrigir-bandeiras.sql
+++
@@ -1,3 +0,0 @@
-UPDATE selecoes SET "codigoPais" = 'kr' WHERE nome = 'Coreia do Sul';
-UPDATE selecoes SET "codigoPais" = 'cz' WHERE nome = 'República Tcheca';
-UPDATE selecoes SET "codigoPais" = 'cw' WHERE nome = 'Curaçao';
